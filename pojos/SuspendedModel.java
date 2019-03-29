ab93b07bfeed4296821d78b8ea530b50cd9511f5
Matthias Lerch <42934778+mle98@users.noreply.github.com>
2019-03-29 18:26:42 +0100

    Added two new POJOs
    BusModel and SuspendedModel added

---
+++ pojos/SuspendedModel.java
@@ -0,0 +1,62 @@
+package POJOs;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "suspended")
+public class SuspendedModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "suspended_id")
+    private int _suspendedId;
+    @ManyToOne
+    private BusModel _busModel;
+    @Column(name = "startdate")
+    private LocalDate _startDate;
+    @Column(name = "enddate")
+    private LocalDate _endDate;
+    @Column(name = "cause")
+    private String _cause;
+
+    public int getSuspendedId() {
+        return _suspendedId;
+    }
+
+    public void setSuspendedId(int suspendedId) {
+        _suspendedId = suspendedId;
+    }
+
+    public BusModel getBusModel() {
+        return _busModel;
+    }
+
+    public void setBusModel(BusModel busModel) {
+        _busModel = busModel;
+    }
+
+    public LocalDate getStartDate() {
+        return _startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        _startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return _endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        _endDate = endDate;
+    }
+
+    public String getCause() {
+        return _cause;
+    }
+
+    public void setCause(String cause) {
+        _cause = cause;
+    }
+}
