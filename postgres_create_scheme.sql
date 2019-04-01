9eb0429649a382d11053b77b23479e8145e7c299
emabee <emanuel.beer@outlook.com>
2019-04-01 19:55:59 +0200

    changed names of most tables from plural to singular version (except "paths" --> is a reserved keyword), added licence_plate_number to "bus" relation

--- postgres_create_scheme.sql
+++ postgres_create_scheme.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS stations (
+CREATE TABLE IF NOT EXISTS station (
     station_id serial,
     station_name varchar(255) not null,
     location_description varchar(255),
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS stations (
 );
 
 
-create table if not exists busses (
+create table if not exists bus (
     bus_id serial,
+	licence_plate_number, varchar(7),
     capacity integer,
     seats integer,
     operational_area varchar(255),
@@ -23,7 +24,7 @@ create table if not exists busses (
 create type category AS enum ('workday', 'schoolday', 'saturday', 'sunday_holiday');
 
 
-create table if not exists timetables (
+create table if not exists timetable (
     timetable_id serial,
     valid_from date,
     valid_to date,
@@ -32,23 +33,23 @@ create table if not exists timetables (
 );
 
 
-create table if not exists shift_days (
+create table if not exists shift_day (
     shift_day_id serial,
     bus_id integer,
     shift_date date,
     shift_category category,
     primary key (shift_day_id),
-    foreign key (bus_id) references busses(bus_id)
+    foreign key (bus_id) references bus(bus_id)
 );
 
-create table if not exists suspensions (
+create table if not exists suspension (
     suspended_id serial,
     bus_id integer,
     start_date date,
     end_date date,
     cause varchar(255),
     primary key (suspended_id),
-    foreign key (bus_id) references busses(bus_id),
+    foreign key (bus_id) references bus(bus_id),
     constraint start_date_before_end_date check (end_date >= start_date)
 );
 
@@ -58,7 +59,7 @@ create table if not exists category_start_times (
     primary key (category_start_time_id)
 );
 
-create table if not exists start_times (
+create table if not exists start_time (
     start_time_id serial,
     category_start_time_id integer,
     start_time time,
@@ -67,14 +68,14 @@ create table if not exists start_times (
     foreign key (category_start_time_id) references category_start_times(category_start_time_id)
 );
 
-create table if not exists routes (
+create table if not exists route (
     route_id serial,
     timetable_id integer,
     route_number varchar(255),
     variation varchar(255),
 
     primary key (route_id),
-    foreign key (timetable_id) references timetables(timetable_id)
+    foreign key (timetable_id) references timetable(timetable_id)
 );
 
 create table if not exists paths (
@@ -85,10 +86,10 @@ create table if not exists paths (
 
     primary key (path_id),
     foreign key (category_start_time_id) references category_start_times(category_start_time_id),
-    foreign key (route_id) references routes(route_id)
+    foreign key (route_id) references route(route_id)
 );
 
-create table if not exists path_stations (
+create table if not exists path_station (
     path_station_id serial,
     path_id integer,
     station_id integer,
@@ -99,14 +100,14 @@ create table if not exists path_stations (
 );
 
 
-create table if not exists route_rides (
+create table if not exists route_ride (
     route_ride_id serial,
     shift_day_id integer,
     start_time_id integer,
     path_id integer,
     primary key (route_ride_id),
-    foreign key (shift_day_id) references shift_days(shift_day_id),
-    foreign key (start_time_id) references start_times(start_time_id),
+    foreign key (shift_day_id) references shift_day(shift_day_id),
+    foreign key (start_time_id) references start_time(start_time_id),
     foreign key (path_id) references paths(path_id)
 );
 
